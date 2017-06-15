7f77ab51d5089797336de41e3552b7086323e58e
karyvdj <kaina51@hotmail.com>
2017-06-15 16:17:55 -0500

    Imprlemtando modal para obtener datos de cada tema y responsive

--- js/main.js
+++ js/main.js
@@ -3,18 +3,24 @@ var api = {
 };
 
 var $listaTemas = $("#lista-temas");
+var $info = $("#info");
+
 
 var cargarPagina = function(){
   $('.modal').modal();
   cargarTemas();
   $("#agregar-btn").click(agregarTema);
   $("#busqueda").submit(filtrarTemas);
+  $(document).on("click",$info,obtenerDatos);
 }
 
-var plantillaTema = "<tr>"+
+var plantillaTema = "<tr data-id='__id__'>"+
           "<td>__autor__</td>"+
           "<td>__mensaje__</td>"+
           "<td>__respuestas__</td>"+
+          "<td>"+
+            "<a class='waves-effect waves-light' id='info' href='#modalInfo'><i class='material-icons'>visibility</i></a>"+
+          "</td>"+
         "</tr>";
 
 var cargarTemas = function () {
@@ -28,7 +34,8 @@ var crearTema = function (tema) {
   var $plantillaFinal = "";
   var $autor = tema.author_name;
   var $mensaje = tema.content;
-  // var $id = tarea.id;
+  var $id = tema.id;
+  // console.log($id)
   var $respuestas = tema.responses_count;
 
   $plantillaFinal += plantillaTema.replace("__autor__", $autor)
@@ -42,21 +49,59 @@ var agregarTema = function (e) {
   e.preventDefault();
   var $obtenerAutor = $("#texto-autor").val();
   var $obtenerMensaje = $("#texto-mensaje").val();
+  var $respuesta = 0;
 
-  $.post(api.url, {author_name: $obtenerAutor, content: $obtenerMensaje }, function (tema) {
+  $.post(api.url, {
+      author_name: $obtenerAutor,
+      content: $obtenerMensaje,
+      responses_count: $respuesta
+     }, function (tema) {
     crearTema(tema);
-
   });
 };
 
 var filtrarTemas = function (e) {
 	e.preventDefault();
 	var $busqueda = $("#buscar").val().toLowerCase();
-  var $autor = tema.author_name;
-	var $temaFiltrados = $autor.filter(function (temas) {
-		return temas.author_name.toLowerCase().indexOf($busqueda) >= 0;
+  var $mensaje = tema.content;
+	var $temaFiltrados = $mensaje.filter(function (temas) { //temas no esta definido
+		return temas.content.toLowerCase().indexOf($busqueda) >= 0;
 	});
 crearTema($temaFiltrados);
 };
 
+function obtenerDatos(){
+  var temaId = $(this).closest("tr").data("id");
+  console.log(temaId);
+  var urlTema = api.url + temaId;
+  console.log(urlTema);
+
+  // $.getJSON(urlTema,function(response){
+  //   var $autor= response.author_name;
+  //   var $mensaje = response.content;
+  //   var $id = response.id;
+  //   console.log($id)
+  //   var $respuestas = response.responses_count;
+  //
+  //   MostrarInfo({
+  //     autor: $autor,
+  //     mensaje: $mensaje,
+  //     id: $id,
+  //     respuesta: $respuestas
+  //   });
+  // });
+}
+//
+// function MostrarInfo(objeto){
+//   var $autor = $("#autor");
+//   var $mensaje = $("#tema");
+//   var $respuesta = $("#respuesta");
+//
+//   $autor.text(objeto.autor);
+//   $mensaje.text(objeto.mensaje);
+//   $respuesta.text(objeto.respuesta);
+//
+// }
+
+// ---------movile---------
 $(document).ready(cargarPagina);
